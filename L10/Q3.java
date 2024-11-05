281d53d3963b5bad9be3114857c1a59b50ffc2f6
Mihir Makwana <mihirpmakwana786@gmail.com>
2024-11-05 20:18:15 +0530

    First Commit

---
+++ L10/Q3.java
@@ -0,0 +1,26 @@
+// Take an array of numbers as input and check if it is an array sorted in ascending order.
+// Eg : { 1, 2, 4, 7 } is sorted in ascending order.
+// {3, 4, 6, 2} is not sorted in ascending order.
+
+
+public class Q3 {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 1, 7}; 
+        
+        boolean isSorted = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false; 
+                break;
+            }
+        }
+
+        if (isSorted) {
+            System.out.println("The array is sorted in ascending order.");
+        } else {
+            System.out.println("The array is not sorted in ascending order.");
+        }
+    }
+}
+
